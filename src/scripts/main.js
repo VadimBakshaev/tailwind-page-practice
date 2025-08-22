f794f0b37bc3758cba683a1290f3cdec2bd9056f
Vadim <vadimm@citydom.ru>
2025-08-22 10:54:59 +0300

    finished page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 import "./../styles/styles.css";
+
+const overlay = document.getElementById("overlay");
+const menu = document.getElementById("menu");
+
+document.getElementById("open").addEventListener("click", () => {
+  overlay.classList.remove("hidden");
+  menu.classList.remove("hidden");
+});
+
+document.getElementById("close").addEventListener("click", () => {
+  menu.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
